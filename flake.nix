5cf0c548511eb864e3e2b4b4132c68a8ab27c23d
Zach Yerrill <26354308+yerrill@users.noreply.github.com>
2024-08-03 00:58:28 -0400

    signs of life

--- flake.nix
+++ flake.nix
@@ -6,19 +6,39 @@
   # https://nixos.wiki/wiki/Home_Manager
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = github:nix-community/home-manager;
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@attrs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  let
+    inherit (self) outputs;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = attrs;
+      specialArgs = { inherit inputs outputs; };
       modules = [ ./hosts/desktop/configuration.nix ];
+    };
+    
+    homeConfigurations."zach" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+	extraSpecialArgs = { inherit inputs outputs;};
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
     };
   };
 }
