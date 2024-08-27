96822d73ab770afc3ceef64e060f83480bce63a4
Zach Yerrill <26354308+yerrill@users.noreply.github.com>
2024-08-26 20:36:40 -0400

    first homemanager / hyprland config migration

--- flake.nix
+++ flake.nix
@@ -1,40 +1,49 @@
 {
-  description = "github:yerrill/nixos config";
+	description = "github:yerrill/nixos config";
 
-  # https://nixos.wiki/wiki/NixOS_modules
-  # https://nixos.wiki/wiki/flakes
+	# https://nixos.wiki/wiki/NixOS_modules
+	# https://nixos.wiki/wiki/flakes
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-  };
+		hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+	};
 
-  outputs = { self, nixpkgs, ... } @ inputs:
-  let
-    inherit (self) outputs;
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    nixosConfigurations = {
-      desktop = nixpkgs.lib.nixosSystem {
-      	system = "${system}";
-	specialArgs = { inherit inputs outputs; };
-	modules = [ ./hosts/desktop/configuration.nix ./modules ];
-      };
+	outputs = { self, nixpkgs, home-manager, ... } @ inputs:
+	let
+		inherit (self) outputs;
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+	in {
+		nixosConfigurations = {
+			desktop = nixpkgs.lib.nixosSystem {
+				system = "${system}";
+				specialArgs = { inherit inputs outputs; };
+				modules = [
+					./hosts/desktop/configuration.nix
+					./modules
+					home-manager.nixosModules.home-manager
+				];
+			};
 
-      laptop = nixpkgs.lib.nixosSystem {
-        system = "${system}";
-	specialArgs = { inherit inputs outputs; };
-	modules = [ ./hosts/laptop/configuration.nix ./modules ];
-      };
-    };
+			laptop = nixpkgs.lib.nixosSystem {
+				system = "${system}";
+				specialArgs = { inherit inputs outputs; };
+				modules = [
+					./hosts/laptop/configuration.nix
+					./modules
+					home-manager.nixosModules.default
+				];
+			};
+		};
 
-    nixosModules = import ./modules;
-  };
+	nixosModules = import ./modules;
+
+	};
 }
